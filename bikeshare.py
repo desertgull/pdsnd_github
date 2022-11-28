edec20d4bb44658266e429ffe4f72fdda6d46c88
desertgull <desertgull@gmail.com>
2022-11-28 10:18:24 -0800

    docs: add programming style information

--- bikeshare.py
+++ bikeshare.py
@@ -15,7 +15,11 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-
+    # Additional commenting
+    # This script takes three inputs as described above
+    # Each input is converted to lower case to compare 
+    # to the available option, that are also in lower case
+    
     print('\nHello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
